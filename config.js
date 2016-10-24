f03ed58ab19fa69d109cc2e3dabcb0f41d4890d9
Alex Bahler <alexbahler@gmail.com>
2016-10-24 02:21:25 +0000

    Use mlab deployment for production db

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
+                            'mongodb://abahler:Mr00ben@ds031157.mlab.com:31157/baskitt' :
                             'mongodb://localhost/shopping-list-dev');
                             
 exports.PORT = process.env.PORT || 8080;
