3cec918cbc3ac510edb5dccd2aa8643d9de7e528
Alex Bahler <alexbahler@gmail.com>
2016-10-27 02:09:45 +0000

    Try port 5000 as default

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
                             'mongodb://abahler:Mr00ben@ds031157.mlab.com:31157/baskitt' :
+                            // 'mongodb://localhost/shopping-list':
                             'mongodb://localhost/shopping-list-dev');
                             
-exports.PORT = process.env.PORT || 8080;
+exports.PORT = process.env.PORT || 5000;
